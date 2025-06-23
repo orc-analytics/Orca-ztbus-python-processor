3d24c83b9eba6719e2c34e58a2a2ce2ab83f90df
Frederick Mannings <fred.mannings@solvicode.com>
2025-06-23 22:30:18 +0100

    Updates

--- processor.py
+++ processor.py
@@ -1,12 +1,18 @@
 from orca_python import Processor, ExecutionParams
 from windows import EveryMinute
-import time
+import datetime as dt
 from windows import AppliedBreaks, EveryMinute
-from quer
+from queries import ReadTelemetryForTripAndTime, ReadTelemParams
+
 proc = Processor("ztbus_analyser")
 
 @proc.algorithm("FindBreakWindows", "1.0.0", EveryMinute)
 def find_when_breaking_windows(params: ExecutionParams) -> None:
     # get telemetry for this window
+    telem = ReadTelemetryForTripAndTime(ReadTelemParams(
+        time_from=dt.datetime.fromtimestamp(params.window.time_from), 
+        time_to = dt.fromtimestamp(params.window.time_to)
+    ))
+    print(telem)
 
     ...
