f0bd08fcc280b4a7b09abde004025193085a1479
Frederick Mannings <fred.mannings@predixus.com>
2025-09-29 11:25:35 +0100

    Orc 48 (#2)
    * Removed the break checking - and reduced metrics down to bus_id level
    
    * Bumped Orca python and integrated metadata support

--- windows.py
+++ windows.py
@@ -1,17 +1,37 @@
-from orca_python import WindowType
+from orca_python import WindowType, MetadataField
+
+trip_id = MetadataField(name="trip_id", description="Unique identifier of the trip")
+bus_id = MetadataField(name="bus_id", description="Unique identifier of the bus")
+route_id = MetadataField(name="route_id", description="Unique identifier for the route")
 
 EveryMinute = WindowType(
     name="EveryMinute", version="1.0.0", description="Triggered every minute"
 )
 
+EveryMinutePerTripPerBus = WindowType(
+    name="EveryMinutePerTripPerBus",
+    version="1.0.0",
+    description="Triggered every minute for an ongoing trip per bus",
+    metadataFields=[trip_id, bus_id, route_id],
+)
+
+TripEnd = WindowType(
+    name="TripEnd",
+    version="1.1.0",
+    description="Triggered when a trip ends",
+    metadataFields=[trip_id, bus_id, route_id],
+)
+
 HaltBrakeApplied = WindowType(
     name="HaltBrakeApplied",
-    version="1.0.0",
+    version="2.1.0",
     description="When the halt brake is applied",
+    metadataFields=[trip_id, bus_id, route_id],
 )
 
 ParkBrakeApplied = WindowType(
     name="ParkBrakeApplied",
-    version="1.0.0",
+    version="2.1.0",
     description="When the park brake is applied",
+    metadataFields=[trip_id, bus_id, route_id],
 )
